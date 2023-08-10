d6c85cf3b194ddaf6f56d6acfd23adbd3004650c
jsina <jsina.maleki@gmail.com>
2023-08-10 16:13:40 +0330

    add config file for getting env constants

---
+++ config.ts
@@ -0,0 +1,4 @@
+import "dotenv/config";
+export const ALCHEMY_TESTNET_RPC_URL =
+  process.env.ALCHEMY_TESTNET_RPC_URL || "";
+export const TESTNET_PRIVATE_KEY = process.env.TESTNET_PRIVATE_KEY || "";
