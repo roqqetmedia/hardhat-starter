4b9a09956fbba8fdb35a0b3448040cd5263b50f9
jsina <jsina.maleki@gmail.com>
2023-08-10 16:13:50 +0330

    setup hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { ALCHEMY_TESTNET_RPC_URL, TESTNET_PRIVATE_KEY } from "./config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: ALCHEMY_TESTNET_RPC_URL || "",
+      accounts: [TESTNET_PRIVATE_KEY || ""],
+    },
+  },
+};
+
+export default config;
